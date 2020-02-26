5937290b4ab9790d9ae86a8ed1f96047bde41fb8
Ankur Anant <ankuranant99@gmail.com>
2020-02-26 16:34:18 +0530

    cust. network hud

--- Assets/Scripts/CustomNetworkManagerHUD.cs
+++ Assets/Scripts/CustomNetworkManagerHUD.cs
@@ -11,4 +11,59 @@ public class CustomNetworkManagerHUD : MonoBehaviour
 
     public InputField port;
 
+    void Awake()
+    {
+        networkManager = GetComponent<NetworkManager>();
+        telepathyTransport = GetComponent<TelepathyTransport>();
+    }
+
+    void Start()
+    {
+        // TODO: take input from InputField
+        NetworkManager.singleton.networkAddress = "http://localhost";
+        telepathyTransport.port = 3000;
+    }
+
+    public void StartHost()
+    {
+        if (!NetworkClient.isConnected && !NetworkServer.active)
+        {
+            if (!NetworkClient.active)
+            {
+                Debug.Log("StartHost: starting server...");
+                networkManager.StartHost();
+            }
+        }
+    }
+
+    public void StopHost()
+    {
+        if(NetworkServer.active && NetworkClient.isConnected)
+        {
+            networkManager.StopHost();
+        }
+    }
+
+    public void StartClient()
+    {
+        if(!NetworkClient.active)
+        {
+            networkManager.StartClient();
+            // TODO: add ip
+        }
+    }
+
+    // cancel connection attempts
+    public void CancelConnection()
+    {
+        if(NetworkClient.isConnected)
+        {
+            networkManager.StopClient();
+        }
+    }
+
+    private string getIp()
+    {
+        if(NetworkClient)
+    }
 }
