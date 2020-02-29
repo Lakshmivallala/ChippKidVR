c73d8d2693f727db24dc6182029193a02b1bbb6f
Ankur Anant <ankuranant99@gmail.com>
2020-02-29 16:13:11 +0530

    ui shit

--- Assets/Scripts/CustomNetworkRoomManager.cs
+++ Assets/Scripts/CustomNetworkRoomManager.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class CustomNetworkRoomManager : NetworkRoomManager
 {
+    public ScrollView lobbyPlayers;
+
     public override void OnGUI()
     {
-        base.OnGUI();
+        if (SceneManager.GetActiveScene().name == RoomScene)
+        {
+            Debug.Log("Ass");
+        }
     }
 }
 
--- Assets/Scripts/CustomNetworkRoomPlayer.cs
+++ Assets/Scripts/CustomNetworkRoomPlayer.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.SceneManagement;
 
 public class CustomNetworkRoomPlayer : NetworkRoomPlayer
 {
-    
+    public override void OnGUI()
+    {
+
+    }
 }
